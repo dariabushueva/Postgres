c8499c5c84701dd11bbafc77f7c7032dc07b48e9
dariabushueva <bushueva84@yandex.ru>
2023-06-28 17:59:04 +0300

    described sql-inserts

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customer_data
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+)
+
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text
+)
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customer_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(50)
+)
